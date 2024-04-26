ef404474b8052aa75fe6261144866f85c7c4132f
jacob <1063010784@qq.com>
2024-04-26 10:56:35 +0800

    udpate package version

--- components/Layout.js
+++ components/Layout.js
@@ -5,13 +5,11 @@ export default function Layout({ children }) {
     <div className="layout">
       <header>
         <Link href="/">
-          <a>
-            <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
-            </h1>
-            <h2>Spread The Joy</h2>
-          </a>
+          <h1>
+            <span>Just Add</span>
+            <span>Marmite</span>
+          </h1>
+          <h2>Spread The Joy</h2>
         </Link>
       </header>
 
